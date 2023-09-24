8972b660c456b62cf5ef3abc789e96cc93c86d34
Richard Suarez <richardmsc24@gmail.com>
2023-09-24 19:48:59 +0200

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>2023 Little Lemon</p>
+      <ul>
+        <li><a href="/terminos">Términos de uso</a></li>
+        <li><a href="/privacidad">Política de privacidad</a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <h2>¡Bienvenido a nuestro sitio web!</h2>
+      <p>Este es el contenido principal de la página.</p>
+    </main>
+  );
+}
+
+export default Main;
